2999c93da0decff46828271c8a97856da864f786
niteshim0 <niteshim111@gmail.com>
2023-11-01 15:37:45 +0530

    authentication,login,sociallogin done

--- app/(site)/components/AuthFom.tsx
+++ app/(site)/components/AuthFom.tsx
@@ -6,7 +6,11 @@ import { useCallback, useState } from "react";
 import { useForm ,FieldValues,SubmitHandler} from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import {BsGithub,BsGoogle} from "react-icons/bs";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+
 type Variant = 'LOGIN'|'REGISTER';//typescript allows the Generic types
+
 const AuthForm =()=> {
 
   const[variant,setVariant] = useState<Variant>('LOGIN');
@@ -38,17 +42,45 @@ const AuthForm =()=> {
     setIsLoading(true);
   
     if (variant === 'REGISTER') {
-      axios.post('/api/register',data);
+      axios.post('/api/register',data)
+      .then(() => {
+        toast.success('Registered successfully!');
+      })
+      .catch(()=> toast.error('Something went wrong!'))
+      .finally(()=> setIsLoading(false));
     }
   
     if (variant === 'LOGIN') {
+      signIn('credentials',{
+        ...data,
+        redirect: false
+      })
+      .then((callback)=>{
+        if(callback?.error){
+          toast.error('Invalid Credentials');
+        }
+        if(callback?.ok && !callback?.error){
+          toast.success('Logged in!');
+        }
+      })
+      .finally(()=> setIsLoading(false));
       // perform actions related to user login using NextAuth SignIn or similar authentication methods
     }
   };
 
   const socialAction = (action: string) => {
     setIsLoading(true);
-    // NextAuth Social SignIn or similar social sign-in process
+    signIn(action,{redirect:false})
+    .then((callback)=>{
+      if(callback?.error){
+        toast.error('Invalid Credentials');
+      }
+      if(callback?.ok && !callback?.error){
+        toast.success('Logged in!');
+      }
+    })
+    .finally(()=>{setIsLoading(false)});
+
   };
   
 
